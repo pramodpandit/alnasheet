042d9afd5ef9304a3d5e4451fab4270fc2cb2cfe
Pramod Pandit <pramod@aarvytechnologies.co.in>
2024-03-01 02:42:27 -0800

    ok

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
-import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:nb_utils/nb_utils.dart';
 import 'package:provider/provider.dart';
 import 'package:alnasheet/controller/login_screen_provider.dart';
@@ -10,7 +8,6 @@ import 'package:alnasheet/view/spalsh_screen.dart';
 void main() async{
   WidgetsFlutterBinding.ensureInitialized();
   await initialize();
-  await dotenv.load();
   runApp(const MyApp());
 }
 
